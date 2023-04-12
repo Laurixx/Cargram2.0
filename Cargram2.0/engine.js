a63edfaf6a2fe8fd5fbb46cb73ba059a1961218b
Laurentiu Robu <43887989+Laurixx@users.noreply.github.com>
2023-04-12 07:33:31 +0100

    Add files via upload

---
+++ Cargram2.0/engine.js
@@ -0,0 +1,63 @@
+const posts = [
+    {
+        name:"Ethan Whites",
+        username:"ethan_wht",
+        location:"Odessa, Texas",
+        avatar:"images/1-avatar.jpg",
+        avatarAlt:"Provile picture of Ethan",
+        featurePost:"images/1.jpg",
+        postAlt:"Dodge challenger RT parked",
+        description:"My Dodge Challenger R/T never fails to turn heads when I'm cruising down the road. The classic muscle car look paired with modern performance features makes for one incredible ride",
+        likes:32,
+        comments:["damn boy that just looks right","SHIT, no more drag racing with you"]
+    },
+    {
+        name:"Liam Brown",
+        username:" liam_brown",
+        location:"Miami",
+        avatar:"images/2-avatar.jpg",
+        avatarAlt:"Provile picture of Liam",
+        featurePost:"images/2.jpg",
+        postAlt: "new rims on liam's Lambo",
+        description:"Just got new rims for my baby, and she's looking hotter than ever.",
+        likes:850,
+        comments:["You bettr dont scratch those Liam","What's this third set of rims in a year?"]
+    },
+    {
+        name:" Ava Williams",
+        username:"ava_smith",
+        location:"California",
+        avatar:"images/3-avatar.jpg",
+        avatarAlt:"Provile picture of Ava",
+        featurePost:"images/3.jpg",
+        postAlt:" Yellow supra not stock , in top of a garage building",
+        description:"Cruising around in my Supra, feeling like a total badass. It may not be the flashiest ride out there, but it's got some serious power under the hood",
+        likes:331,
+        comments:["For all the haters ,I saw whats under that hood you better take your hands of the keys"]
+    },
+    {
+        name: "Emma Davis",
+        username:"emma_rose",
+        location:"Frankfurt, Germany",
+        avatar:"images/4-avatar.jpg",
+        avatarAlt:"Provile picture of Emma",
+        featurePost:"images/4.jpg",
+        postAlt: "Emma's Corvette , purple wraped with some blue lines on the sunset",
+        description:"I mean, it's not everyday you get to drive a Corvette, especially one that's custom wrapped. People can't help but stare as I fly by, and I gotta say, it's kind of fun being the center of attention.",
+        likes:268,
+        comments:["You know there are about 20 of those in Germany Yeah ?"]
+    },
+    {
+        name:"Caleb Johnson",
+        username:"caleb_johnson",
+        location:"Cornwall, UK",
+        avatar:"images/5-avatar.jpg",
+        avatarAlt:"Provile picture of Caleb",
+        featurePost:"images/5.jpg",
+        postAlt: "Classic ferray low to the ground on the streets of Cornwall ",
+        description:"When I'm behind the wheel of my Ferrari, I feel like I'm living in a movie. The low profile and sleek lines just scream luxury and power. And honestly, it's not just a car - it's a whole vibe.",
+        likes:97,
+        comments:["she will hate you at a speed bump"]
+    }
+
+]
